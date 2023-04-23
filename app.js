5f507d9f83bde53202ce95c5c7ba74678d7485e1
Elliot Markley <Elliotmarkley.dev@gmail.com>
2023-04-23 17:47:05 -0400

    small update

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/failure', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+    console.log('Server is running on port 3000!.');
 });
 
 
